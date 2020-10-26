d9bdfb2e0ba79b10426ebed154892da55475368b
Seth Anderson <sdande02@gmail.com>
2020-10-25 19:04:44 -0700

    dll

--- LinkedList.js
+++ LinkedList.js
@@ -70,66 +70,6 @@ class DoublyLinkedList {
 
 
 
-class Node {
-    constructor(val) {
-        this.val = val;
-        this.next = null;
-        this.prev = null;
-    }
-}
-
-
-// class Node {
-//     constructor(val) {
-//         this.val = val;
-//         this.next = null;
-//     }
-// }
-
-
-
-
-class DoublyLinkedList {
-    constructor() {
-        this.length = 0;
-        this.head = null;
-        this.tail = null;
-    }
-
-    push(val) {
-        const newNode = new Node(val);
-        if (!this.head) {
-            this.head = newNode;
-            this.tail = this.head;
-        } else {
-            this.tail.next = newNode;
-            newNode.prev = this.tail;
-            this.tail = newNode;
-        }
-        this.length++;
-
-        return this;
-    }
-    pop() {
-        if (!this.head) return undefined;
-        const poppedNode = this.tail;
-        if (this.length === 1) {
-            this.head = null;
-            this.tail = null;
-        } else {
-            this.tail = poppedNode.prev;
-            this.tail.next = null;
-            poppedNode.prev = null; 
-        }
-        this.length--;
-        return poppedNode;
-    }
-
-
-}
-
-
-
 
 
 
