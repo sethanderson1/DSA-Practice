f0cf5593fc6f2701a702c1fe05535d9b069ec0fa
Seth Anderson <sethdanderson17@gmail.com>
2021-07-13 22:56:27 -0700

    Add rotate array

--- src/algos/rotateArray.js
+++ src/algos/rotateArray.js
@@ -1,33 +1,62 @@
 const rotateArray = (arr, k) => {
-  k = k % arr.length;
-  let count = 0;
+  reverseArr(arr, 0, arr.length - 1)
+  reverseArr(arr, 0, k - 1)
+  reverseArr(arr, k, arr.length - 1)
+  return arr
+}
 
-    let count = 0;
-    for (let start = 0; count < arr.length; start++) {
-      let current = start;
-      let prev = arr[start];
-      while (true) {
-        let next = (current + k) % arr.length;
-        let temp = arr[next];
-        arr[next] = prev;
-        prev = temp;
-        current = next;
-        count++;
-        if (current === start) {
-          break;
-        }
-      }
-    }
-    return arr;
+const reverseArr = (arr, l, end) => {
+  let r = end
+  let i = 0
+  while (l < r) {
+    let tmp = arr[l]
+    arr[l] = arr[end - i]
+    arr[r] = tmp
+    l++
+    r--
+    i++
   }
+}
 
-//                 i
-//          t3     t1    t2
 const arr = [0, 1, 2, 3, 4, 5, 6];
-//           i
-const result = rotateArray(arr, 2);
+const result = rotateArray(arr, 1);
 console.log("result", result);
 
 module.exports = {
   rotateArray,
 };
+
+
+// const rotateArray = (arr, k) => {
+//   k = k % arr.length;
+//   let count = 0;
+
+//     let count = 0;
+//     for (let start = 0; count < arr.length; start++) {
+//       let cur = start;
+//       let prev = arr[start];
+//       while (true) {
+//         let next = (cur + k) % arr.length;
+//         let temp = arr[next];
+//         arr[next] = prev;
+//         prev = temp;
+//         cur = next;
+//         count++;
+//         if (cur === start) {
+//           break;
+//         }
+//       }
+//     }
+//     return arr;
+//   }
+
+// //                 i
+// //          t3     t1    t2
+// const arr = [0, 1, 2, 3, 4, 5, 6];
+// //           i
+// const result = rotateArray(arr, 2);
+// console.log("result", result);
+
+// module.exports = {
+//   rotateArray,
+// };
